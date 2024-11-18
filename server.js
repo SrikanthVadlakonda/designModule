ab397706c38b5e507b972bed68e977d0ec450235
srikanth369 <vadlakondasrikanth136@gmail.com>
2024-11-18 23:02:25 +0530

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use("/api/designs", designRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
